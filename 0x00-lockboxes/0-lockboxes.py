9072bcb681b084f9880a5c75baa554e9c9f65513
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-04-21 15:55:40 -0500

    (0x00) style: pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes (list of numbers) inside a list can be unlocked.
+    Determines if all the boxes (list of numbers) inside a list
+    can be unlocked.
+
     Each box can contain an integer indicating the index of another box.
 
     The first box is unlocked
@@ -11,20 +14,16 @@ def canUnlockAll(boxes):
         boxes (list): list of lists.
     """
 
-    i = 0;
+    i = 0
     unlocked = [boxes[0]]
 
     def unlock(boxes, box, unlocked):
-        print(boxes)
-        print(unlocked)
-        input()
         for key in box:
             if boxes[key] or not boxes[key]:
                 if boxes[key] not in unlocked:
                     unlocked.append(boxes[key])
                 else:
-                    continue;
-
+                    continue
 
     while i < len(unlocked):
         unlock(boxes, unlocked[i], unlocked)
