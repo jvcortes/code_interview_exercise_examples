5476a342b62836edb748f80f682d62f9d265a677
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-04-21 23:17:06 -0500

    (0x00) fix: task 0
    Fixed infinite loop when a list contained duplicate keys.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -16,28 +16,34 @@ def canUnlockAll(boxes):
 
     i = 0
     unlocked = []
+    keys = []
 
     if boxes:
-        unlocked.append(boxes[0])
+        unlocked.append(boxes[i])
     else:
         return False
 
-    def unlock(boxes, box, unlocked):
+    while len(boxes) != len(unlocked):
+        box = unlocked[i]
+        if set(box).issubset(set(keys)):
+            if i == len(unlocked) - 1:
+                break;
+            else:
+                i += 1
+                continue
         for key in box:
+            if key in keys:
+                continue
             if key >= len(boxes):
                 continue
-
             if boxes[key] or not boxes[key]:
-                if unlocked.count(boxes[key]) < boxes.count(boxes[key]):
-                    unlocked.append(boxes[key])
-                else:
-                    continue
+                unlocked.append(boxes[key])
+                keys.append(key)
 
-    while i < len(unlocked):
-        unlock(boxes, unlocked[i], unlocked)
+        if i == len(unlocked) - 1:
+            break;
         i += 1
 
-    if len(unlocked) == len(boxes):
+    if len(boxes) == len(unlocked):
         return True
-    else:
-        return False
+    return False
