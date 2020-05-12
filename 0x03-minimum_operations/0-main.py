2c74cf907272eaa3833ce9a9b5cfb7a87705a1d2
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-05-11 23:17:13 -0500

    (0x0#) initial commit

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 16
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 2
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 13
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 3
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 5
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 17
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 18
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """
+    Returns the number of minimum operations to go from a single character
+    'H' to n 'H' characters.
+
+    The valid operations are composed of:
+        - Copy all the text.
+        - Paste
+
+    Parameters:
+        n (int): number of 'H' characters to determine the amount of minimum
+        operations.
+    """
+
+    d = 2
+    paste = 0
+    copy_all = 0
+
+    if n in (0, 1):
+        return 0
+
+    m = n
+
+    while (True):
+        if m % d == 0:
+            m = m / d
+            d = 2
+            paste += 1
+            copy_all += 1
+        else:
+            copy_all += 1
+            d += 1
+
+        if m == 1:
+            break
+
+    return paste + copy_all
