655279544b77895ac4d4f42957b0ae523a510df6
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-08-28 12:17:28 -0500

    (0x04) fix: task 0
    Fixed issue where a stable grid would be printed after doing its initial
    sum.

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -43,11 +43,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < 3; j++)
 			grid1[i][j] += grid2[i][j];
 
-	do {
+	while (is_unstable(grid1)) {
 		printf("=\n");
 		print_grid(grid1);
 		topple(grid1);
-	} while (is_unstable(grid1));
+	}
 }
 
 /**
