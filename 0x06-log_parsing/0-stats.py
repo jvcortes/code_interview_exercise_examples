5eac5e2ca6d89c56e3fbcc875ea8e5cc1e6790ed
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-09-02 14:56:27 -0500

    (0x06) fix: task 0
    Removed word length validation.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,7 +9,6 @@ Every 10 lines, the size of the read contents and the count for every found
 status code will be printed to the standard output.
 """
 import re
-import signal
 import sys
 
 
@@ -19,22 +18,24 @@ def get_info_from_line(line, info):
     if status_match:
         status = status_match.group(2)
     else:
-        return
+        return False
 
     if status not in ("200", "301", "400", "401", "403", "404", "405", "500"):
-        return
+        return False
 
     filesize_match = re.search(r"(.*?\s)?\d*\s(\d*)$", line)
     if filesize_match:
         filesize = filesize_match.group(2)
     else:
-        return
+        return False
 
     if status in info["status"]:
         info["status"][status] += 1
     else:
         info["status"][status] = 1
     info["total_size"] += int(filesize)
+
+    return True
 
 
 if __name__ == "__main__":
@@ -46,8 +47,7 @@ if __name__ == "__main__":
 
     try:
         for line in sys.stdin:
-            if len(line.split()) > 2:
-                get_info_from_line(line.rstrip('\n'), info)
+            if get_info_from_line(line.rstrip('\n'), info):
                 count += 1
 
             if count == 10:
@@ -55,8 +55,6 @@ if __name__ == "__main__":
                 for key in sorted(info["status"]):
                     print("{}: {}".format(key, info["status"][key]))
                 count = 0
-    except Exception:
-        pass
     finally:
         print("File size: {}".format(info["total_size"]))
         for key in sorted(info["status"]):
