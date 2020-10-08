24d1219c4cf6180f7262c39fb4d6fb27135904a2
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-10-08 00:02:14 -0500

    (0x0B) fix: header file
    Added missing function declarations.

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -2,4 +2,8 @@
 #define MENGER
 
 void menger(int level);
+void build_level_one_sponge(char **sponge, char symbol);
+void recurse(char **sponge, int idx, int level, int len, int nxt);
+void print_sponge(char **sponge, int len);
+void free_sponge(char **sponge, int max_len);
 #endif /* ifndef MENGER */
